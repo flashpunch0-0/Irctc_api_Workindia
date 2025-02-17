7d44d903e1fa4b9ecce35cdfad0cf5c6ef3e70f5
KRATIK_BOHRA <kratikbohra5@gmail.com>
2025-02-17 17:48:38 +0530

    postgress sql connection established using sequelize

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const cors = require("cors");
+const sequelize = require("./config/db");
+
 require("dotenv").config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+sequelize
+  .sync()
+  .then(() => {
+    console.log("Tables syncronized");
+  })
+  .catch((err) => console.log("Error creating tables", err));
 
 app.listen(3010, () => {
   console.log(`Server is running on post 3010`);
