5c8bf7a7f1b44e8990706c8ac99c4339d738a6fd
KRATIK_BOHRA <kratikbohra5@gmail.com>
2025-02-17 23:28:55 +0530

    apikey check function crated

---
+++ middleware/apikeyMiddleware.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+const apiKeyMiddleware = async (req, res) => {
+  const apikey = req.header("x-api-key");
+  const validapikey = process.env.ADMIN_API_KEY;
+  if (!apikey || apikey !== validapikey) {
+    return res.status(400).json({ message: "Invalid api key" });
+  }
+  console.log("api key verifed");
+  next();
+};
+module.exports = apiKeyMiddleware;
