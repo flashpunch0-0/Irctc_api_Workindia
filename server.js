947f719eb61e96ac0fae3b7e849037f4495edb9f
KRATIK_BOHRA <kratikbohra5@gmail.com>
2025-02-17 17:33:40 +0530

    initial commit ( downloaded dependencies and server.js)

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.listen(3010, () => {
+  console.log(`Server is running on post 3010`);
+});
